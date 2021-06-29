0534f9337b680dc4f8d38a390b496a44b722b4a0
xXDeSXx <31136719+xXDeSXx@users.noreply.github.com>
2021-06-29 12:36:20 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -2,4 +2,10 @@
 # Формат входных данных: дано три натуральных числа. Гарантируется, что отрезки с данными длинами образуют треугольник.
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
-# TODO: your code here
+side_a = int(input("Insert first side of triangle>>"))
+side_b = int(input("Insert second side of triangle>>"))
+side_c = int(input("Insert third side of triangle>>"))
+if side_a == side_b or side_a == side_c or side_b == side_c:
+    print("Triangle with side A=", side_a, ",side B=", side_b, ",side C=", side_c,"is isosceles")
+else:
+    print("Triangle with side A=", side_a, ",side B=", side_b, ",side C=", side_c, "isn't isosceles")
